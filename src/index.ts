844ce8694cc886c4c293b068b065c8b01564154a
gauravPanditr <gauravpandit7856@gmail.com>
2026-06-26 12:35:58 +0530

    connected scoket

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
-
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import {Server} from "socket.io"
+import { createServer } from "node:http";
+import cors from "cors";
 const app = express();
 import apiRouter from './routes/index';
 const PORT=process.env.PORT ||3000;
 
+const server= createServer(app)
+const io=new Server(server,{
+  cors:{
+    origin:'*',
+    methods:['GET','POST']
+  }
+})
+io.on('connection',()=>{
+  console.log("a user connected");
+  
+})
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.use('/api', apiRouter);
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
