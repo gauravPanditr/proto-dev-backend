14cad1f1b750f3ee0eed59b2ec7fe99394b1b1be
gauravPanditr <gauravpandit7856@gmail.com>
2026-08-11 14:24:02 +0530

    fix create project controller

--- src/controller/projectController.ts
+++ src/controller/projectController.ts
@@ -6,11 +6,17 @@ import { ProjectParams } from "../types/project";
 
 
 
-export const createProject=async(res:Response)=>{
-    const projectId=await projectCreateService();
-      return res.json({message:"Project created",data:projectId});
+export const createProject = async (
+ 
+  res: Response
+) => {
+  const projectId = await projectCreateService();
 
-}
+  return res.status(201).json({
+    message: "Project created",
+    data: projectId,
+  });
+};
 export const getProjectTree=async(req:Request<ProjectParams>,res:Response)=>{
   const tree=await getProjectTreeService(req.params.projectId);
   return res.status(200).json({
