b9b59d61f6f5cff3e65666f04861f02f94f4d933
gauravPanditr <gauravpandit7856@gmail.com>
2026-08-16 13:46:03 +0530

    fix ping controller

--- src/controller/pingController.ts
+++ src/controller/pingController.ts
@@ -1,5 +1,7 @@
-import {  Response } from "express";
+import { Request, Response } from "express";
 
-export async function pingCheck(res:Response) {
-    return res.status(200).json({ message: 'pong' });
+export async function pingCheck(_req: Request, res: Response) {
+  return res.status(200).json({
+    message: "pong",
+  });
 }
